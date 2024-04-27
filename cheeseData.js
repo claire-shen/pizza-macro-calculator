a318e56d84efb74afe18ab222bee1570ca4f7bc3
claire-shen <claireshen1516@gmail.com>
2024-04-27 16:35:27 -0400

    bug fixes

--- cheeseData.js
+++ cheeseData.js
@@ -95,3 +95,99 @@ export const cheeseDataT = {
     }
 };
 
+export const cheeseDataCO = {
+    'Personal': {
+        'handTossed': {
+            'CLight': { calories: 60, protein: 6, fats: 4, carbs: 2 },
+            'CRegular': { calories: 100, protein: 8, fats: 7, carbs: 2 },
+            'CExtra': { calories: 140, protein: 12, fats: 10, carbs: 2 },
+            'CDouble': { calories: 200, protein: 18, fats: 14, carbs: 4 },
+            'CTriple': { calories: 260, protein: 22, fats: 18, carbs: 4 }
+        }
+    },
+    'Small': {
+        'glutenFreeCrust': {
+            'CLight': { calories: 210, protein: 18, fats: 13.5, carbs: 3 },
+            'CRegular': { calories: 420, protein: 33, fats: 27, carbs: 6 },
+            'CExtra': { calories: 570, protein: 48, fats: 39, carbs: 9 },
+            'CDouble': { calories: 570, protein: 48, fats: 39, carbs: 9 },
+            'CTriple': { calories: 720, protein: 60, fats: 48, carbs: 12 }
+        },
+        'crunchyThinCrust': {
+            'CLight': { calories: 200, protein: 16, fats: 14, carbs: 4 },
+            'CRegular': { calories: 400, protein: 36, fats: 28, carbs: 8 },
+            'CExtra': { calories: 560, protein: 48, fats: 40, carbs: 8 },
+            'CDouble': { calories: 560, protein: 48, fats: 40, carbs: 8 },
+            'CTriple': { calories: 720, protein: 60, fats: 48, carbs: 12 }
+        },
+        'originalHandTossed': {
+            'CLight': { calories: 210, protein: 18, fats: 15, carbs: 6 },
+            'CRegular': { calories: 420, protein: 36, fats: 27, carbs: 6 },
+            'CExtra': { calories: 600, protein: 48, fats: 36, carbs: 12 },
+            'CDouble': { calories: 600, protein: 48, fats: 36, carbs: 12 },
+            'CTriple': { calories: 720, protein: 60, fats: 48, carbs: 12 }
+        }
+    },
+    'Medium': {
+        'originalHandTossed': {
+            'CLight': { calories: 280, protein: 24, fats: 20, carbs: 8 },
+            'CRegular': { calories: 640, protein: 48, fats: 40, carbs: 8 },
+            'CExtra': { calories: 800, protein: 72, fats: 56, carbs: 16 },
+            'CDouble': { calories: 800, protein: 72, fats: 56, carbs: 16 },
+            'CTriple': { calories: 1040, protein: 88, fats: 72, carbs: 16 }
+        },
+        'crunchyThinCrust': {
+            'CLight': { calories: 280, protein: 24, fats: 20, carbs: 4 },
+            'CRegular': { calories: 600, protein: 52, fats: 40, carbs: 12 },
+            'CExtra': { calories: 840, protein: 68, fats: 56, carbs: 12 },
+            'CDouble': { calories: 840, protein: 68, fats: 56, carbs: 12 },
+            'CTriple': { calories: 1040, protein: 84, fats: 68, carbs: 16 }
+        },
+        'handmadePan': {
+            'CLight': { calories: 560, protein: 48, fats: 40, carbs: 8 },
+            'CRegular': { calories: 640, protein: 48, fats: 40, carbs: 8 },
+            'CExtra': { calories: 720, protein: 56, fats: 48, carbs: 16 },
+            'CDouble': { calories: 880, protein: 72, fats: 56, carbs: 16 },
+            'CTriple': { calories: 960, protein: 80, fats: 64, carbs: 16 }
+        }
+    },
+    'Large': {
+        'originalHandTossed': {
+            'CLight': { calories: 400, protein: 32, fats: 28, carbs: 8 },
+            'CRegular': { calories: 880, protein: 72, fats: 56, carbs: 16 },
+            'CExtra': { calories: 1120, protein: 96, fats: 80, carbs: 16 },
+            'CDouble': { calories: 1120, protein: 96, fats: 80, carbs: 16 },
+            'CTriple': { calories: 1440, protein: 120, fats: 96, carbs: 24 }
+        },
+        'brooklynCrust': {
+            'CLight': { calories: 420, protein: 36, fats: 27, carbs: 6 },
+            'CRegular': { calories: 840, protein: 72, fats: 60, carbs: 12 },
+            'CExtra': { calories: 1140, protein: 96, fats: 78, carbs: 18 },
+            'CDouble': { calories: 1140, protein: 96, fats: 78, carbs: 18 },
+            'CTriple': { calories: 1440, protein: 120, fats: 96, carbs: 24 }
+        },
+        'crunchyThinCrust': {
+            'CLight': { calories: 400, protein: 32, fats: 28, carbs: 8 },
+            'CRegular': { calories: 880, protein: 72, fats: 56, carbs: 16 },
+            'CExtra': { calories: 1120, protein: 96, fats: 80, carbs: 16 },
+            'CDouble': { calories: 1120, protein: 96, fats: 80, carbs: 16 },
+            'CTriple': { calories: 1440, protein: 120, fats: 96, carbs: 24 }
+        }
+    },
+    'XLarge': {
+        'originalHandTossed': {
+            'CLight': { calories: 500, protein: 40, fats: 35, carbs: 10 },
+            'CRegular': { calories: 1100, protein: 90, fats: 70, carbs: 20 },
+            'CExtra': { calories: 1500, protein: 120, fats: 100, carbs: 20 },
+            'CDouble': { calories: 1500, protein: 120, fats: 100, carbs: 20 },
+            'CTriple': { calories: 1800, protein: 150, fats: 120, carbs: 30 }
+        },
+        'brooklynCrust': {
+            'CLight': { calories: 540, protein: 42, fats: 36, carbs: 6 },
+            'CRegular': { calories: 1080, protein: 90, fats: 72, carbs: 18 },
+            'CExtra': { calories: 1500, protein: 126, fats: 102, carbs: 24 },
+            'CDouble': { calories: 1500, protein: 126, fats: 102, carbs: 24 },
+            'CTriple': { calories: 1860, protein: 156, fats: 126, carbs: 30 }
+        }
+    }
+};
