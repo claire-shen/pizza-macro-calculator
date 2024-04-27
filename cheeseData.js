0a36bcc3a7f09509d5edc5bd0ac3b92274eef9b9
claire-shen <claireshen1516@gmail.com>
2024-04-27 16:26:41 -0400

    added data structure to store cheese values for toppings pizzas

---
+++ cheeseData.js
@@ -0,0 +1,97 @@
+export const cheeseDataT = {
+    'Personal': {
+        'handTossed': {
+            'TLight': { calories: 60, protein: 6, fats: 4, carbs: 2 },
+            'TRegular': { calories: 100, protein: 8, fats: 7, carbs: 2 },
+            'TExtra': { calories: 140, protein: 12, fats: 10, carbs: 2 },
+            'TDouble': { calories: 200, protein: 18, fats: 14, carbs: 4 },
+            'TTriple': { calories: 260, protein: 22, fats: 18, carbs: 4 }
+        }
+    },
+    'Small': {
+        'glutenFreeCrust': {
+            'TLight': { calories: 210, protein: 18, fats: 13.5, carbs: 3 },
+            'TRegular': { calories: 300, protein: 24, fats: 18, carbs: 6 },
+            'TExtra': { calories: 420, protein: 33, fats: 27, carbs: 6 },
+            'TDouble': { calories: 570, protein: 48, fats: 39, carbs: 9 },
+            'TTriple': { calories: 720, protein: 60, fats: 48, carbs: 12 }
+        },
+        'crunchyThinCrust': {
+            'TLight': { calories: 200, protein: 16, fats: 14, carbs: 4 },
+            'TRegular': { calories: 280, protein: 24, fats: 20, carbs: 4 },
+            'TExtra': { calories: 400, protein: 36, fats: 28, carbs: 8 },
+            'TDouble': { calories: 560, protein: 48, fats: 40, carbs: 8 },
+            'TTriple': { calories: 720, protein: 60, fats: 48, carbs: 12 }
+        },
+        'originalHandTossed': {
+            'TLight': { calories: 210, protein: 18, fats: 15, carbs: 6 },
+            'TRegular': { calories: 300, protein: 24, fats: 18, carbs: 6 },
+            'TExtra': { calories: 420, protein: 36, fats: 27, carbs: 6 },
+            'TDouble': { calories: 600, protein: 48, fats: 36, carbs: 12 },
+            'TTriple': { calories: 720, protein: 60, fats: 48, carbs: 12 }
+        }
+    },
+    'Medium': {
+        'originalHandTossed': {
+            'TLight': { calories: 280, protein: 24, fats: 20, carbs: 8 },
+            'TRegular': { calories: 400, protein: 32, fats: 28, carbs: 8 },
+            'TExtra': { calories: 640, protein: 48, fats: 40, carbs: 8 },
+            'TDouble': { calories: 800, protein: 72, fats: 56, carbs: 16 },
+            'TTriple': { calories: 1040, protein: 88, fats: 72, carbs: 16 }
+        },
+        'crunchyThinCrust': {
+            'TLight': { calories: 280, protein: 24, fats: 20, carbs: 4 },
+            'TRegular': { calories: 400, protein: 36, fats: 28, carbs: 8 },
+            'TExtra': { calories: 600, protein: 52, fats: 40, carbs: 12 },
+            'TDouble': { calories: 840, protein: 68, fats: 56, carbs: 12 },
+            'TTriple': { calories: 1040, protein: 84, fats: 68, carbs: 16 }
+        },
+        'handmadePan': {
+            'TLight': { calories: 560, protein: 48, fats: 40, carbs: 8 },
+            'TRegular': { calories: 640, protein: 48, fats: 40, carbs: 8 },
+            'TExtra': { calories: 720, protein: 56, fats: 48, carbs: 16 },
+            'TDouble': { calories: 880, protein: 72, fats: 56, carbs: 16 },
+            'TTriple': { calories: 960, protein: 80, fats: 64, carbs: 16 }
+        }
+    },
+    'Large': {
+        'originalHandTossed': {
+            'TLight': { calories: 400, protein: 32, fats: 28, carbs: 8 },
+            'TRegular': { calories: 560, protein: 48, fats: 40, carbs: 8 },
+            'TExtra': { calories: 880, protein: 72, fats: 56, carbs: 16 },
+            'TDouble': { calories: 1120, protein: 96, fats: 80, carbs: 16 },
+            'TTriple': { calories: 1440, protein: 120, fats: 96, carbs: 24 }
+        },
+        'brooklynCrust': {
+            'TLight': { calories: 420, protein: 36, fats: 27, carbs: 6 },
+            'TRegular': { calories: 600, protein: 48, fats: 36, carbs: 12 },
+            'TExtra': { calories: 840, protein: 72, fats: 60, carbs: 12 },
+            'TDouble': { calories: 1140, protein: 96, fats: 78, carbs: 18 },
+            'TTriple': { calories: 1440, protein: 120, fats: 96, carbs: 24 }
+        },
+        'crunchyThinCrust': {
+            'TLight': { calories: 400, protein: 32, fats: 28, carbs: 8 },
+            'TRegular': { calories: 560, protein: 48, fats: 40, carbs: 8 },
+            'TExtra': { calories: 880, protein: 72, fats: 56, carbs: 16 },
+            'TDouble': { calories: 1120, protein: 96, fats: 80, carbs: 16 },
+            'TTriple': { calories: 1440, protein: 120, fats: 96, carbs: 24 }
+        }
+    },
+    'XLarge': {
+        'originalHandTossed': {
+            'TLight': { calories: 500, protein: 40, fats: 35, carbs: 10 },
+            'TRegular': { calories: 700, protein: 60, fats: 50, carbs: 10 },
+            'TExtra': { calories: 1100, protein: 90, fats: 70, carbs: 20 },
+            'TDouble': { calories: 1500, protein: 120, fats: 100, carbs: 20 },
+            'TTriple': { calories: 1800, protein: 150, fats: 120, carbs: 30 }
+        },
+        'brooklynCrust': {
+            'TLight': { calories: 900, protein: 70, fats: 60, carbs: 10 },
+            'TRegular': { calories: 1200, protein: 100, fats: 80, carbs: 20 },
+            'TExtra': { calories: 1800, protein: 150, fats: 120, carbs: 30 },
+            'TDouble': { calories: 2500, protein: 210, fats: 170, carbs: 40 },
+            'TTriple': { calories: 3100, protein: 260, fats: 210, carbs: 50 }
+        }
+    }
+};
+
--- dominos.js
+++ dominos.js
@@ -1,3 +1,5 @@
+import { cheeseDataT } from './cheeseData.js';
+
 var totalCalories = 0;
 var totalProtein = 0;
 var totalCarbs = 0; 
@@ -8,6 +10,10 @@ var selectedCrust = '';
 var selectedSize = '';
 var crustSelected = false;
 var sizeSelected = false;
+
+var cheeseOrToppings = '';
+var cheeseLevelToppings = '';
+var cheeseLevelCheese= '';
 
 var crustSizeInfo = {
     'Personal': {
@@ -43,15 +49,14 @@ sizeSelect.addEventListener('change', function () {
     selectedSize = sizeSelect.value;
     sizeSelected = true; 
     console.log(selectedSize);
-
-    selectedCrust = ''; // Reset selectedCrust when size changes
-    selectedCrust.innerHTML = '';
-
+    
+    selectedCrust = ''; 
+    
     totalCalories = 0;
     totalProtein = 0;
     totalFats = 0;
     totalCarbs = 0;
-
+    
     if (selectedSize === 'Personal') {
         addCrustOptions([{ text: 'Select your crust', value: 'crustPlaceholder' },
         { text: 'Hand Tossed', value: 'handTossed' }]);
@@ -60,26 +65,26 @@ sizeSelect.addEventListener('change', function () {
         { text: 'Original Hand Tossed', value: 'originalHandTossed' },
         { text: 'Crunchy Thin Crust', value: 'crunchyThinCrust' },
         { text: 'Gluten Free Crust', value: 'glutenFreeCrust' }
-        ]);
-    } else if (selectedSize === 'Medium') {
-        addCrustOptions([{ text: 'Select your crust', value: 'crustPlaceholder' },
-        { text: 'Original Hand Tossed', value: 'originalHandTossed' },
-        { text: 'Crunchy Thin Crust', value: 'crunchyThinCrust' },
-        { text: 'Handmade Pan', value: 'handmadePan' }
-        ]);
-    } else if (selectedSize === 'Large') {
-        addCrustOptions([{ text: 'Select your crust', value: 'crustPlaceholder' },
-        { text: 'Original Hand Tossed', value: 'originalHandTossed' },
-        { text: 'Brooklyn Crust', value: 'brooklynCrust' },
-        { text: 'Crunchy Thin Crust', value: 'crunchyThinCrust' }
-        ]);
-    } else if (selectedSize === 'XLarge') {
-        addCrustOptions([{ text: 'Select your crust', value: 'crustPlaceholder' },
-        { text: 'Original Hand Tossed', value: 'originalHandTossed' },
-        { text: 'Brooklyn Crust', value: 'brooklynCrust' }
-        ]);
-    } 
-    document.getElementById('crust-dropdown').style.display = 'block';
+    ]);
+} else if (selectedSize === 'Medium') {
+    addCrustOptions([{ text: 'Select your crust', value: 'crustPlaceholder' },
+    { text: 'Original Hand Tossed', value: 'originalHandTossed' },
+    { text: 'Crunchy Thin Crust', value: 'crunchyThinCrust' },
+    { text: 'Handmade Pan', value: 'handmadePan' }
+]);
+} else if (selectedSize === 'Large') {
+    addCrustOptions([{ text: 'Select your crust', value: 'crustPlaceholder' },
+    { text: 'Original Hand Tossed', value: 'originalHandTossed' },
+    { text: 'Brooklyn Crust', value: 'brooklynCrust' },
+    { text: 'Crunchy Thin Crust', value: 'crunchyThinCrust' }
+]);
+} else if (selectedSize === 'XLarge') {
+    addCrustOptions([{ text: 'Select your crust', value: 'crustPlaceholder' },
+    { text: 'Original Hand Tossed', value: 'originalHandTossed' },
+    { text: 'Brooklyn Crust', value: 'brooklynCrust' }
+]);
+} 
+document.getElementById('crust-dropdown').style.display = 'block';
 });
 
 function addCrustOptions(crustOptions) {
@@ -127,24 +132,32 @@ cheesePizzaBtn.addEventListener('click', function() {
         document.getElementById('chosen-pizza').innerHTML = "Please select a size and crust first";
         return;
     }
+    cheeseOrToppings = 'cheese';
     document.getElementById('chosen-pizza').innerHTML = "You have selected a cheese-only pizza";
     cheesePizzaBtn.style.backgroundColor = '#3b3e41';
     toppingsPizzaBtn.style.backgroundColor = '#e27704'
-
+    
     var cheesePizzaDiv = document.createElement('div');
     cheesePizzaDiv.classList.add('select-cheese'); 
     cheesePizzaDiv.innerHTML = `
-        <select id="cheese-select" class="form-select font4 p-2" aria-label="Default select">
-            <option class="grey-border" selected>Select your size</option>
-            <option value="CLight">Light Cheese</option>
-            <option value="CRegular">Regular Cheese</option>
-            <option value="CExtra">Extra Cheese</option>
-            <option value="CDouble">Double Cheese</option>
-            <option value="CTriple">Triple Cheese</option>
-        </select>
+    <select id="cheese-select" class="form-select font4 p-2" aria-label="Default select">
+    <option class="grey-border" selected>Select your size</option>
+    <option value="CLight">Light Cheese</option>
+    <option value="CRegular">Regular Cheese</option>
+    <option value="CExtra">Extra Cheese</option>
+    <option value="CDouble">Double Cheese</option>
+    <option value="CTriple">Triple Cheese</option>
+    </select>
     `;
     pizzaOption.innerHTML = '';
     pizzaOption.appendChild(cheesePizzaDiv);
+    
+    var cheeseListener = document.getElementById('cheese-select');
+    cheeseListener.addEventListener('change', function() {
+        cheeseLevelCheese = cheeseListener.value;
+        console.log(cheeseLevelCheese);
+        calculateCheeseCO(cheeseLevelCheese); 
+    });
 });
 
 toppingsPizzaBtn.addEventListener('click', function() {
@@ -152,24 +165,401 @@ toppingsPizzaBtn.addEventListener('click', function() {
         document.getElementById('chosen-pizza').innerHTML = "Please select a size and crust first";
         return;
     }
+    cheeseOrToppings = 'toppings';
     document.getElementById('chosen-pizza').innerHTML = "You have selected a pizza with toppings";
     toppingsPizzaBtn.style.backgroundColor = '#3b3e41';
     cheesePizzaBtn.style.backgroundColor = '#e27704'
-
+    
     var toppingPizzaDiv = document.createElement('div');
     toppingPizzaDiv.classList.add('select-cheese-toppings'); 
     toppingPizzaDiv.innerHTML = `
-        <select id="cheese-select" class="form-select font4 p-2" aria-label="Default select">
-            <option class="Tgrey-border" selected>Select your cheese</option>
-            <option value="TLight">Light Cheese</option>
-            <option value="TRegular">Regular Cheese</option>
-            <option value="TExtra">Extra Cheese</option>
-            <option value="TDouble">Double Cheese</option>
-            <option value="TTriple">Triple Cheese</option>
-        </select>
+    <select id="cheese-select-toppings" class="form-select font4 p-2" aria-label="Default select">
+    <option class="Tgrey-border" selected>Select your cheese</option>
+    <option value="TLight">Light Cheese</option>
+    <option value="TRegular">Regular Cheese</option>
+    <option value="TExtra">Extra Cheese</option>
+    <option value="TDouble">Double Cheese</option>
+    <option value="TTriple">Triple Cheese</option>
+    </select>
     `;
     pizzaOption.innerHTML = '';
     pizzaOption.appendChild(toppingPizzaDiv);
+    
+    var cheeseListener = document.getElementById('cheese-select-toppings');
+    cheeseListener.addEventListener('change', function() {
+        cheeseLevelToppings = cheeseListener.value;
+        console.log(cheeseLevelToppings);
+        calculateCheeseT(cheeseLevelToppings); 
+    });
+    
 });
+
+
+function calculateCheeseT(level) {
+    const info = cheeseDataT[selectedSize][selectedCrust][cheeseLevelToppings];
+    totalCalories += info.calories;
+    totalProtein += info.protein;
+    totalFats += info.fats;
+    totalCarbs += info.carbs;
+    console.log(totalCalories);
+}
+
+
+function calculateCheeseCO(level) {
+    if (selectedSize == 'Personal' && selectedCrust == 'handTossed') {
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 60;
+            totalProtein += 6;
+            totalFats += 4;
+            totalCarbs += 2;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 100;
+            totalProtein += 8;
+            totalFats += 7;
+            totalCarbs += 2;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 140;
+            totalProtein += 12;
+            totalFats += 10;
+            totalCarbs += 2;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 200;
+            totalProtein += 18;
+            totalFats += 14;
+            totalCarbs += 4;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 260;
+            totalProtein += 22;
+            totalFats += 18;
+            totalCarbs += 4;
+        } 
+    }
+    
+    if (selectedSize == 'Small' && selectedCrust == 'glutenFreeCrust') {
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 210;
+            totalProtein += 18;
+            totalFats += 13.5;
+            totalCarbs += 3;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 420;
+            totalProtein += 33;
+            totalFats += 27;
+            totalCarbs += 6;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 570;
+            totalProtein += 48;
+            totalFats += 39;
+            totalCarbs += 9;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 570;
+            totalProtein += 48;
+            totalFats += 39;
+            totalCarbs += 9;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 720;
+            totalProtein += 60;
+            totalFats += 48;
+            totalCarbs += 12;
+        }
+    }
+    
+    
+    if (selectedSize == 'Small' && selectedCrust == 'crunchyThinCrust') {
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 200;
+            totalProtein += 16;
+            totalFats += 14;
+            totalCarbs += 4;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 400;
+            totalProtein += 36;
+            totalFats += 28;
+            totalCarbs += 8;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 560;
+            totalProtein += 48;
+            totalFats += 40;
+            totalCarbs += 8;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 560;
+            totalProtein += 48;
+            totalFats += 40;
+            totalCarbs += 8;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 720;
+            totalProtein += 60;
+            totalFats += 48;
+            totalCarbs += 12;
+        }
+    }
+    
+    
+    if (selectedSize == 'Small' && selectedCrust == 'originalHandTossed') {
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 210;
+            totalProtein += 18;
+            totalFats += 15;
+            totalCarbs += 6;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 420;
+            totalProtein += 36;
+            totalFats += 27;
+            totalCarbs += 6;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 600;
+            totalProtein += 48;
+            totalFats += 36;
+            totalCarbs += 12;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 600;
+            totalProtein += 48;
+            totalFats += 36;
+            totalCarbs += 12;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 720;
+            totalProtein += 60;
+            totalFats += 48;
+            totalCarbs += 12;
+        }            
+    }        
+    
+    if (selectedSize === 'Medium' && selectedCrust === 'originalHandTossed') {
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 280;
+            totalProtein += 24;
+            totalFats += 20;
+            totalCarbs += 8;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 640;
+            totalProtein += 48;
+            totalFats += 40;
+            totalCarbs += 8;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 800;
+            totalProtein += 72;
+            totalFats += 56;
+            totalCarbs += 16;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 800;
+            totalProtein += 72;
+            totalFats += 56;
+            totalCarbs += 16;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 1040;
+            totalProtein += 88;
+            totalFats += 72;
+            totalCarbs += 16;
+        }            
+    }
+    
+    if ((selectedSize == 'Medium') && (selectedCrust == 'crunchyThinCrust')){
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 280;
+            totalProtein += 24;
+            totalFats += 20;
+            totalCarbs += 4;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 600;
+            totalProtein += 52;
+            totalFats += 40;
+            totalCarbs += 12;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 840;
+            totalProtein += 68;
+            totalFats += 56;
+            totalCarbs += 12;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 840;
+            totalProtein += 68;
+            totalFats += 56;
+            totalCarbs += 12;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 1040;
+            totalProtein += 84;
+            totalFats += 68;
+            totalCarbs += 16;
+        }            
+    } 
+    
+    if ((selectedSize == 'Medium') && (selectedCrust == 'handmadePan')){
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 560;
+            totalProtein += 48;
+            totalFats += 40;
+            totalCarbs += 8;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 640;
+            totalProtein += 48;
+            totalFats += 40;
+            totalCarbs += 8;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 720;
+            totalProtein += 56;
+            totalFats += 48;
+            totalCarbs += 16;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 880;
+            totalProtein += 72;
+            totalFats += 56;
+            totalCarbs += 16;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 960;
+            totalProtein += 80;
+            totalFats += 64;
+            totalCarbs += 16;
+        } 
+    }
+    
+    if ((selectedSize == 'Large') && (selectedCrust == 'originalHandTossed')){
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 400;
+            totalProtein += 32;
+            totalFats += 28;
+            totalCarbs += 8;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 880;
+            totalProtein += 72;
+            totalFats += 56;
+            totalCarbs += 16;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 1120;
+            totalProtein += 96;
+            totalFats += 80;
+            totalCarbs += 16;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 1120;
+            totalProtein += 96;
+            totalFats += 80;
+            totalCarbs += 16;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 1440;
+            totalProtein += 120;
+            totalFats += 96;
+            totalCarbs += 24;
+        }                          
+    }
+    
+    if ((selectedSize == 'Large') && (selectedCrust == 'brooklynCrust')){
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 420;
+            totalProtein += 36;
+            totalFats += 27;
+            totalCarbs += 6;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 840;
+            totalProtein += 72;
+            totalFats += 60;
+            totalCarbs += 12;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 1140;
+            totalProtein += 96;
+            totalFats += 78;
+            totalCarbs += 18;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 1140;
+            totalProtein += 96;
+            totalFats += 78;
+            totalCarbs += 18;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 1440;
+            totalProtein += 120;
+            totalFats += 96;
+            totalCarbs += 24;
+        }     
+    }
+    
+    if ((selectedSize == 'Large') && (selectedCrust == 'crunchyThinCrust')){
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 400;
+            totalProtein += 32;
+            totalFats += 28;
+            totalCarbs += 8;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 880;
+            totalProtein += 72;
+            totalFats += 56;
+            totalCarbs += 16;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 1120;
+            totalProtein += 96;
+            totalFats += 80;
+            totalCarbs += 16;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 1120;
+            totalProtein += 96;
+            totalFats += 80;
+            totalCarbs += 16;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 1440;
+            totalProtein += 120;
+            totalFats += 96;
+            totalCarbs += 24;
+        }                           
+    }
+    
+    if ((selectedSize == 'XLarge') && (selectedCrust == 'originalHandTossed')) {
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 500;
+            totalProtein += 40;
+            totalFats += 35;
+            totalCarbs += 10;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 1100;
+            totalProtein += 90;
+            totalFats += 70;
+            totalCarbs += 20;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 1500;
+            totalProtein += 120;
+            totalFats += 100;
+            totalCarbs += 20;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 1500;
+            totalProtein += 120;
+            totalFats += 100;
+            totalCarbs += 20;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 1800;
+            totalProtein += 150;
+            totalFats += 120;
+            totalCarbs += 30;
+        }                
+    }
+    
+    if ((selectedSize == 'XLarge') && (selectedCrust == 'brooklynCrust')) {
+        if (cheeseLevel == 'CLight') {
+            totalCalories += 540;
+            totalProtein += 42;
+            totalFats += 36;
+            totalCarbs += 6;
+        } else if (cheeseLevel == 'CRegular') {
+            totalCalories += 1080;
+            totalProtein += 90;
+            totalFats += 72;
+            totalCarbs += 18;
+        } else if (cheeseLevel == 'CExtra') {
+            totalCalories += 1500;
+            totalProtein += 126;
+            totalFats += 102;
+            totalCarbs += 24;
+        } else if (cheeseLevel == 'CDouble') {
+            totalCalories += 1500;
+            totalProtein += 126;
+            totalFats += 102;
+            totalCarbs += 24;
+        } else if (cheeseLevel == 'CTriple') {
+            totalCalories += 1860;
+            totalProtein += 156;
+            totalFats += 126;
+            totalCarbs += 30;
+        }               
+    }
+}
+
+
+
+
+
 
 
